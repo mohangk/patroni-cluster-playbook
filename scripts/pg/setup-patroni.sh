40b50a0835fc298c2e8b133c520706cd54e6bc20
Mohan Krishan <mohankrishnan@google.com>
2020-10-09 04:51:56 +0000

    Current WIP patroni setup steps

--- scripts/pg/setup-patroni.sh
+++ scripts/pg/setup-patroni.sh
@@ -1,12 +1,15 @@
 sudo apt-get install python3-pip python3-psycopg2
 sudo pip3 install patroni[etcd]
-#cp patroni.server to /etc/systemd/system/patroni.service
+
+cp patroni.service to /etc/systemd/system/patroni.service
+
 mkdir /etc/patroni
-#cp pg-patroni-1.yml to /etc/patroni/patroni.yml
+cp pg-patroni-1.yml to /etc/patroni/patroni.yml
+chown -R postgres:postgres /etc/patroni
 
 #In psql
 CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'password'
-ALTER USER postgres WITH ENCRYPTED PASSWORD 'paswword'
+ALTER USER postgres WITH ENCRYPTED PASSWORD 'password'
 
 
 Questions:
