f3b2bc846b7ae2aa1ecb0da08561bb98f2da9191
Mark Statham <mstatham@google.com>
2020-10-16 15:03:48 +0000

    Add instance groups to ILB backend

--- scripts/pg/create-pg-cluster-ilb.sh
+++ scripts/pg/create-pg-cluster-ilb.sh
@@ -8,9 +8,10 @@ CLUSTER_NAME=$1
 REGION=${2:-us-central1}
 ROLES="primary replica"
 
+
 for ROLE in $ROLES; do
-	BE_SVC="$CLUSTER_NAME-$ROLE-backend"
-	FWD_RULE="$CLUSTER_NAME-$ROLE-fwding-rule"
+	BE_SVC="$CLUSTER_NAME-$ROLE"
+	FWD_RULE="$CLUSTER_NAME-$ROLE"
 
 	# 1. Create backend-service
 	gcloud compute backend-services create $BE_SVC \
@@ -28,4 +29,17 @@ for ROLE in $ROLES; do
 	  --region=$REGION \
 	  --backend-service=$BE_SVC \
 	  --backend-service-region=$REGION
+done
+
+#3. List the cluster members based on the cluster label to determine which region and zone they are deployed in
+ZONES=$(gcloud compute instances list --filter="labels.cluster:$CLUSTER_NAME" --format="text" | grep zone: | cut -f9 -d\/ | xargs)
+for ROLE in $ROLES; do
+	BE_SVC="$CLUSTER_NAME-$ROLE"
+	for ZONE in $ZONES; do
+		IG_NAME="$CLUSTER_NAME-$ZONE-ig"
+		gcloud compute backend-services add-backend $BE_SVC \
+		--region=$REGION \
+		--instance-group=$IG_NAME \
+		--instance-group-zone=$ZONE
+	done
 done
