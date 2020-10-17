9b0403ceac4a6cd8eb91dd179041f69faec68901
Mark Statham <mstatham@google.com>
2020-10-17 08:15:09 +0000

    create image in a version specific family

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -2,6 +2,17 @@
 
 # $1 - name of the instance to create image from
 # $2 - base name of the image
+
+if [[ $# -ne 2 ]]; then
+    echo "$0 [instance] [image family]"
+    exit 2
+fi
+
 printf -v date '%(%Y%m%d%H%M)T\n' -1
-gcloud compute instances stop $1 --zone us-central1-a
-gcloud compute images create $2-$date --source-disk=$1 #--zone=us-central1-a
+ZONE=$(gcloud compute instances list --filter="labels.pgimage:true" --format="text" | grep zone: | cut -f9 -d\/ | xargs)
+if [ -z $ZONE ]; then
+    echo "couldn't fine image"
+else
+    gcloud compute instances stop $1 --zone $ZONE
+    gcloud compute images create $2-$date --source-disk=$1 --family=$2-image --source-disk-zone=$ZONE
+fi
