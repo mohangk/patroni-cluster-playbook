3c482aede89046dddfbdad5276f44fd2d0385f2e
Mohan Krishan <mohankrishnan@google.com>
2020-11-13 05:17:43 +0000

    Remove disk when deleting instances use for image creation

--- scripts/pg/create-pg-img-instance.sh
+++ scripts/pg/create-pg-img-instance.sh
@@ -14,7 +14,6 @@ gcloud compute  instances create $NAME \
 	--boot-disk-size=10GB \
 	--boot-disk-type=pd-standard \
 	--boot-disk-device-name=$NAME \
-	--no-boot-disk-auto-delete \
 	--tags=$NAME,image \
 	--labels=pgimage=true \
 	--scopes=https://www.googleapis.com/auth/compute.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/trace.append,https://www.googleapis.com/auth/devstorage.read_write \
